0a001cabf33cade36d8371484f2da3af45977a76
Rodrigo de Castro Souza <103949333+srodrigodecastro@users.noreply.github.com>
2022-05-20 21:39:25 -0300

    Horas teste

--- aula12/pratica/ex014/script.js
+++ aula12/pratica/ex014/script.js
@@ -2,7 +2,7 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = 0 //data.getHours()
+    var hora = 15 //data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     
     if (hora >= 0 && hora <=5) {
