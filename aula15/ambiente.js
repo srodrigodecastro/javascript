c3d7950d42a11c190732f5f4373b057fdf9ca749
Rodrigo de Castro Souza <103949333+srodrigodecastro@users.noreply.github.com>
2022-06-03 15:05:37 -0300

    arrays

---
+++ aula15/ambiente.js
@@ -0,0 +1,10 @@
+//Arrays - Vetores
+
+let num = [5, 8, 9, 2, 3]
+num [5] = 6
+num.push(1)
+//num.sort()
+
+console.log(`O nosso vetor tem ${num.length} posições`)
+console.log(`Nosso vertor é o ${num}`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
